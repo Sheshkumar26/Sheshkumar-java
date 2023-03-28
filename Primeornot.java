c2dfe60d4e5482cd8ad2b3c8ef5dddc1295c4382
Shesh23 <94338880+Sheshkumar26@users.noreply.github.com>
2023-03-28 14:50:56 +0530

    Add files via upload

---
+++ Primeornot.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Primeornot {
+
+    public static void main(String[] args) {
+       Scanner sky=new Scanner(System.in);
+        int n=sky.nextInt();
+        if(n%2!=0){
+            System.out.println("yes");
+        }
+        
+        else {
+            System.out.println("no");}
+        
+    }
+}
